f7e6199a41253ec3d7923d76c2c9d74c23d1a3c7
Gabby Florendo <gbonici@deakin.edu.au>
2020-09-03 01:36:37 +0800

    Add database models

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import User, Issue, Car
 
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Issue)
+admin.site.register(Car)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+class User (models.Model):
+
+    email = models.EmailField()
+    password = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+class Car (models.Model):
+
+    make = models.CharField(max_length=50)
+    model = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.make}: {self.model}"
+
+class Issue (models.Model):
+
+    RECALL = "RC"
+    ISSUE = "IC"
+    ISSUE_TYPE_CHOICES = [
+        (RECALL, "recall"),
+        (ISSUE, "issue")
+    ]
+
+    car = models.ForeignKey(Car, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    type = models.CharField(
+        max_length=2,
+        choices=ISSUE_TYPE_CHOICES,
+        default=ISSUE
+        )
+    
+    def __str__(self):
+        return self.title
