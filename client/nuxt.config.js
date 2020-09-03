7a179463cebb5ee06212e771f2f2b4d553973c7c
Gabby Florendo <gbonici@deakin.edu.au>
2020-09-03 17:00:13 +0800

    Add client configuration for api connection

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -61,7 +61,9 @@ export default {
   ** Axios module configuration
   ** See https://axios.nuxtjs.org/options
   */
-    axios: {},
+    axios: {
+        baseURL: "http://localhost:8000/api"
+    },
     /*
   ** Build configuration
   ** See https://nuxtjs.org/api/configuration-build/
