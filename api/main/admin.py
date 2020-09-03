f49777b6f790b5d5f66d718e476bdc51e0a5acc3
Gabby Florendo <gbonici@deakin.edu.au>
2020-09-03 16:26:00 +0800

    Remove custom User model

--- api/main/admin.py
+++ api/main/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from .models import User, Issue, Car
+from .models import Issue, Car
 
 # Register your models here.
 
-admin.site.register(User)
 admin.site.register(Issue)
 admin.site.register(Car)
--- api/main/models.py
+++ api/main/models.py
@@ -1,15 +1,5 @@
 from django.db import models
 
-class User (models.Model):
-
-    email = models.EmailField()
-    password = models.CharField(max_length=50)
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-
-    def __str__(self):
-        return f"{self.first_name} {self.last_name}"
-
 class Car (models.Model):
 
     make = models.CharField(max_length=50)
