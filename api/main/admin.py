c71bc705efa688555325ffddbaf12da284e777b3
Gabby Florendo <gbonici@deakin.edu.au>
2020-09-03 17:22:39 +0800

    Add Make to admin

--- api/main/admin.py
+++ api/main/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Issue, Car
+from .models import Make, Issue, Car
 
 # Register your models here.
 
 admin.site.register(Issue)
 admin.site.register(Car)
+admin.site.register(Make)
