3749930a84e32d4d13898a385ab1075f261f991c
Gabby Florendo <gbonici@deakin.edu.au>
2020-09-26 21:25:32 +0800

    Add searching for Makes

--- client/pages/makes/index.vue
+++ client/pages/makes/index.vue
@@ -10,16 +10,42 @@
                 Add Make
             </b-button>
         </template>
-        <b-table
-            striped
-            hover
-            selectable
-            responsive
-            borderless
-            :fields="fields"
-            :items="makes"
-            @row-clicked="onRowClick"
-        />
+        <b-row>
+            <b-input-group>
+                <b-form-input
+                    v-model="searchTerm"
+                    placeholder="Search Makes"
+                    @keyup.enter="onSearch"
+                />
+                <b-input-group-append>
+                    <b-button
+                        variant="primary"
+                        @click="onSearch"
+                    >
+                        Search
+                    </b-button>
+                </b-input-group-append>
+            </b-input-group>
+        </b-row>
+        <b-row>
+            <b-table
+                v-if="makes.length > 0"
+                striped
+                hover
+                selectable
+                responsive
+                borderless
+                :fields="fields"
+                :items="makes"
+                @row-clicked="onRowClick"
+            />
+            <h2
+                v-else
+                class="text-muted"
+            >
+                <em>There are no makes found using search term "{{ lastSearchTerm }}"</em>
+            </h2>
+        </b-row>
         <b-row align-h="center">
             <b-pagination
                 v-model="curPage"
@@ -55,6 +81,8 @@ export default {
             fields: ["name"],
             perPage: process.env.paginationItemsPerPage,
             curPage: 1,
+            searchTerm: "",
+            lastSearchTerm: "",
             makes: []
         };
     },
@@ -79,6 +107,23 @@ export default {
                 this.reports = result.results;
                 this.nextPage = result.next;
                 this.prevPage = result.previous;
+            } catch (err) {
+                this.$toasted.global.defaultError({
+                    msg: err
+                });
+            }
+        },
+        async onSearch (evt) {
+            let searchUrl = "/api/makes/";
+            searchUrl = this.searchTerm.length > 0 ? `${searchUrl}?search=${this.searchTerm}` : searchUrl;
+            try {
+                const result = await this.$axios.$get(searchUrl);
+                this.makes = result.results;
+                this.next = result.next;
+                this.prevPage = result.previous;
+                this.contentCount = result.count;
+
+                this.lastSearchTerm = this.searchTerm;
             } catch (err) {
                 this.$toasted.global.defaultError({
                     msg: err
